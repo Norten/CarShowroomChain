a7671088cd3bfcdd3dfb76bf84de0050f7c89aa7
Nort <kuchciakkamil@gmail.com>
2015-09-29 00:04:58 +0200

    modyfikacja comboBoxów, możliwość wyboru tylko z listy comboBoxa

--- CarShowroomChain/FormNewCar.Designer.cs
+++ CarShowroomChain/FormNewCar.Designer.cs
@@ -78,87 +78,90 @@
             this.panelBackground.Controls.Add(this.labelModel);
             this.panelBackground.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelBackground.Location = new System.Drawing.Point(0, 0);
-            this.panelBackground.Margin = new System.Windows.Forms.Padding(2);
+            this.panelBackground.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panelBackground.Name = "panelBackground";
-            this.panelBackground.Padding = new System.Windows.Forms.Padding(15, 16, 15, 16);
-            this.panelBackground.Size = new System.Drawing.Size(510, 253);
+            this.panelBackground.Padding = new System.Windows.Forms.Padding(20, 20, 20, 20);
+            this.panelBackground.Size = new System.Drawing.Size(680, 311);
             this.panelBackground.TabIndex = 0;
             // 
             // label
             // 
             this.label.AutoSize = true;
-            this.label.Location = new System.Drawing.Point(418, 110);
-            this.label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label.Location = new System.Drawing.Point(557, 135);
             this.label.Name = "label";
-            this.label.Size = new System.Drawing.Size(28, 13);
+            this.label.Size = new System.Drawing.Size(35, 17);
             this.label.TabIndex = 22;
             this.label.Text = "PLN";
             this.label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // comboBoxShop
             // 
+            this.comboBoxShop.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxShop.FormattingEnabled = true;
-            this.comboBoxShop.Location = new System.Drawing.Point(57, 138);
-            this.comboBoxShop.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxShop.Location = new System.Drawing.Point(76, 170);
+            this.comboBoxShop.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBoxShop.Name = "comboBoxShop";
-            this.comboBoxShop.Size = new System.Drawing.Size(148, 21);
+            this.comboBoxShop.Size = new System.Drawing.Size(196, 24);
             this.comboBoxShop.TabIndex = 21;
             // 
             // comboBoxEngine
             // 
+            this.comboBoxEngine.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxEngine.FormattingEnabled = true;
-            this.comboBoxEngine.Location = new System.Drawing.Point(57, 107);
-            this.comboBoxEngine.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxEngine.Location = new System.Drawing.Point(76, 132);
+            this.comboBoxEngine.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBoxEngine.Name = "comboBoxEngine";
-            this.comboBoxEngine.Size = new System.Drawing.Size(149, 21);
+            this.comboBoxEngine.Size = new System.Drawing.Size(197, 24);
             this.comboBoxEngine.TabIndex = 20;
             // 
             // comboBoxSeries
             // 
+            this.comboBoxSeries.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxSeries.FormattingEnabled = true;
-            this.comboBoxSeries.Location = new System.Drawing.Point(59, 22);
-            this.comboBoxSeries.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxSeries.Location = new System.Drawing.Point(79, 27);
+            this.comboBoxSeries.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBoxSeries.Name = "comboBoxSeries";
-            this.comboBoxSeries.Size = new System.Drawing.Size(146, 21);
+            this.comboBoxSeries.Size = new System.Drawing.Size(193, 24);
             this.comboBoxSeries.TabIndex = 19;
             // 
             // labelSeries
             // 
             this.labelSeries.AutoSize = true;
-            this.labelSeries.Location = new System.Drawing.Point(19, 24);
-            this.labelSeries.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelSeries.Location = new System.Drawing.Point(25, 30);
             this.labelSeries.Name = "labelSeries";
-            this.labelSeries.Size = new System.Drawing.Size(31, 13);
+            this.labelSeries.Size = new System.Drawing.Size(41, 17);
             this.labelSeries.TabIndex = 18;
             this.labelSeries.Text = "Seria";
             this.labelSeries.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // comboBoxModel
             // 
+            this.comboBoxModel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxModel.FormattingEnabled = true;
-            this.comboBoxModel.Location = new System.Drawing.Point(59, 50);
-            this.comboBoxModel.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxModel.Location = new System.Drawing.Point(79, 62);
+            this.comboBoxModel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBoxModel.Name = "comboBoxModel";
-            this.comboBoxModel.Size = new System.Drawing.Size(146, 21);
+            this.comboBoxModel.Size = new System.Drawing.Size(193, 24);
             this.comboBoxModel.TabIndex = 6;
             // 
             // comboBoxColor
             // 
+            this.comboBoxColor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxColor.FormattingEnabled = true;
-            this.comboBoxColor.Location = new System.Drawing.Point(57, 79);
-            this.comboBoxColor.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxColor.Location = new System.Drawing.Point(76, 97);
+            this.comboBoxColor.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBoxColor.Name = "comboBoxColor";
-            this.comboBoxColor.Size = new System.Drawing.Size(148, 21);
+            this.comboBoxColor.Size = new System.Drawing.Size(196, 24);
             this.comboBoxColor.TabIndex = 7;
             // 
             // buttonAccept
             // 
             this.buttonAccept.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonAccept.AutoSize = true;
-            this.buttonAccept.Location = new System.Drawing.Point(373, 215);
-            this.buttonAccept.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonAccept.Location = new System.Drawing.Point(480, 260);
+            this.buttonAccept.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonAccept.Name = "buttonAccept";
-            this.buttonAccept.Size = new System.Drawing.Size(59, 23);
+            this.buttonAccept.Size = new System.Drawing.Size(96, 33);
             this.buttonAccept.TabIndex = 16;
             this.buttonAccept.Text = "Akceptuj";
             this.buttonAccept.UseVisualStyleBackColor = true;
@@ -168,10 +171,10 @@
             // 
             this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonCancel.AutoSize = true;
-            this.buttonCancel.Location = new System.Drawing.Point(436, 215);
-            this.buttonCancel.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonCancel.Location = new System.Drawing.Point(580, 260);
+            this.buttonCancel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(56, 23);
+            this.buttonCancel.Size = new System.Drawing.Size(76, 33);
             this.buttonCancel.TabIndex = 17;
             this.buttonCancel.Text = "Anuluj";
             this.buttonCancel.UseVisualStyleBackColor = true;
@@ -179,38 +182,39 @@
             // 
             // textBoxPrice
             // 
-            this.textBoxPrice.Location = new System.Drawing.Point(342, 107);
-            this.textBoxPrice.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxPrice.Location = new System.Drawing.Point(456, 132);
+            this.textBoxPrice.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxPrice.MaxLength = 50;
             this.textBoxPrice.Name = "textBoxPrice";
-            this.textBoxPrice.Size = new System.Drawing.Size(72, 20);
+            this.textBoxPrice.Size = new System.Drawing.Size(95, 22);
             this.textBoxPrice.TabIndex = 15;
             // 
             // comboBoxCarBody
             // 
+            this.comboBoxCarBody.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxCarBody.FormattingEnabled = true;
-            this.comboBoxCarBody.Location = new System.Drawing.Point(342, 79);
-            this.comboBoxCarBody.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxCarBody.Location = new System.Drawing.Point(456, 97);
+            this.comboBoxCarBody.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBoxCarBody.Name = "comboBoxCarBody";
-            this.comboBoxCarBody.Size = new System.Drawing.Size(148, 21);
+            this.comboBoxCarBody.Size = new System.Drawing.Size(196, 24);
             this.comboBoxCarBody.TabIndex = 14;
             // 
             // comboBoxGearbox
             // 
+            this.comboBoxGearbox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxGearbox.FormattingEnabled = true;
-            this.comboBoxGearbox.Location = new System.Drawing.Point(342, 50);
-            this.comboBoxGearbox.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxGearbox.Location = new System.Drawing.Point(456, 62);
+            this.comboBoxGearbox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBoxGearbox.Name = "comboBoxGearbox";
-            this.comboBoxGearbox.Size = new System.Drawing.Size(148, 21);
+            this.comboBoxGearbox.Size = new System.Drawing.Size(196, 24);
             this.comboBoxGearbox.TabIndex = 13;
             // 
             // labelPrice
             // 
             this.labelPrice.AutoSize = true;
-            this.labelPrice.Location = new System.Drawing.Point(255, 110);
-            this.labelPrice.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelPrice.Location = new System.Drawing.Point(340, 135);
             this.labelPrice.Name = "labelPrice";
-            this.labelPrice.Size = new System.Drawing.Size(32, 13);
+            this.labelPrice.Size = new System.Drawing.Size(41, 17);
             this.labelPrice.TabIndex = 12;
             this.labelPrice.Text = "Cena";
             this.labelPrice.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -218,30 +222,29 @@
             // labelCarBody
             // 
             this.labelCarBody.AutoSize = true;
-            this.labelCarBody.Location = new System.Drawing.Point(255, 81);
-            this.labelCarBody.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelCarBody.Location = new System.Drawing.Point(340, 100);
             this.labelCarBody.Name = "labelCarBody";
-            this.labelCarBody.Size = new System.Drawing.Size(54, 13);
+            this.labelCarBody.Size = new System.Drawing.Size(69, 17);
             this.labelCarBody.TabIndex = 11;
             this.labelCarBody.Text = "Nadwozie";
             this.labelCarBody.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // comboBoxFuel
             // 
+            this.comboBoxFuel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxFuel.FormattingEnabled = true;
-            this.comboBoxFuel.Location = new System.Drawing.Point(342, 21);
-            this.comboBoxFuel.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxFuel.Location = new System.Drawing.Point(456, 26);
+            this.comboBoxFuel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBoxFuel.Name = "comboBoxFuel";
-            this.comboBoxFuel.Size = new System.Drawing.Size(148, 21);
+            this.comboBoxFuel.Size = new System.Drawing.Size(196, 24);
             this.comboBoxFuel.TabIndex = 10;
             // 
             // labelGearbox
             // 
             this.labelGearbox.AutoSize = true;
-            this.labelGearbox.Location = new System.Drawing.Point(255, 53);
-            this.labelGearbox.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelGearbox.Location = new System.Drawing.Point(340, 65);
             this.labelGearbox.Name = "labelGearbox";
-            this.labelGearbox.Size = new System.Drawing.Size(84, 13);
+            this.labelGearbox.Size = new System.Drawing.Size(110, 17);
             this.labelGearbox.TabIndex = 5;
             this.labelGearbox.Text = "Skrzynia biegów";
             this.labelGearbox.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -249,10 +252,9 @@
             // labelFuel
             // 
             this.labelFuel.AutoSize = true;
-            this.labelFuel.Location = new System.Drawing.Point(255, 24);
-            this.labelFuel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelFuel.Location = new System.Drawing.Point(340, 30);
             this.labelFuel.Name = "labelFuel";
-            this.labelFuel.Size = new System.Drawing.Size(38, 13);
+            this.labelFuel.Size = new System.Drawing.Size(48, 17);
             this.labelFuel.TabIndex = 4;
             this.labelFuel.Text = "Paliwo";
             this.labelFuel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -260,10 +262,9 @@
             // labelEnginePower
             // 
             this.labelEnginePower.AutoSize = true;
-            this.labelEnginePower.Location = new System.Drawing.Point(19, 138);
-            this.labelEnginePower.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelEnginePower.Location = new System.Drawing.Point(25, 170);
             this.labelEnginePower.Name = "labelEnginePower";
-            this.labelEnginePower.Size = new System.Drawing.Size(34, 13);
+            this.labelEnginePower.Size = new System.Drawing.Size(43, 17);
             this.labelEnginePower.TabIndex = 3;
             this.labelEnginePower.Text = "Sklep";
             this.labelEnginePower.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -271,10 +272,9 @@
             // labelEngine
             // 
             this.labelEngine.AutoSize = true;
-            this.labelEngine.Location = new System.Drawing.Point(19, 110);
-            this.labelEngine.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelEngine.Location = new System.Drawing.Point(25, 135);
             this.labelEngine.Name = "labelEngine";
-            this.labelEngine.Size = new System.Drawing.Size(32, 13);
+            this.labelEngine.Size = new System.Drawing.Size(41, 17);
             this.labelEngine.TabIndex = 2;
             this.labelEngine.Text = "Silnik";
             this.labelEngine.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -282,10 +282,9 @@
             // labelColor
             // 
             this.labelColor.AutoSize = true;
-            this.labelColor.Location = new System.Drawing.Point(19, 81);
-            this.labelColor.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelColor.Location = new System.Drawing.Point(25, 100);
             this.labelColor.Name = "labelColor";
-            this.labelColor.Size = new System.Drawing.Size(31, 13);
+            this.labelColor.Size = new System.Drawing.Size(41, 17);
             this.labelColor.TabIndex = 1;
             this.labelColor.Text = "Kolor";
             this.labelColor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -293,22 +292,21 @@
             // labelModel
             // 
             this.labelModel.AutoSize = true;
-            this.labelModel.Location = new System.Drawing.Point(19, 53);
-            this.labelModel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelModel.Location = new System.Drawing.Point(25, 65);
             this.labelModel.Name = "labelModel";
-            this.labelModel.Size = new System.Drawing.Size(36, 13);
+            this.labelModel.Size = new System.Drawing.Size(46, 17);
             this.labelModel.TabIndex = 0;
             this.labelModel.Text = "Model";
             this.labelModel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // FormNewCar
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(510, 253);
+            this.ClientSize = new System.Drawing.Size(680, 311);
             this.Controls.Add(this.panelBackground);
-            this.Margin = new System.Windows.Forms.Padding(2);
-            this.MinimumSize = new System.Drawing.Size(492, 292);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MinimumSize = new System.Drawing.Size(650, 349);
             this.Name = "FormNewCar";
             this.Text = "Nowy samochód";
             this.panelBackground.ResumeLayout(false);
