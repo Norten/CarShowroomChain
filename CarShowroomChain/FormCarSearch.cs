6f7d4480876b29fb6d41e020292b79b3e3dbdae5
Agata <agakax@gmail.com>
2015-09-29 16:45:13 +0200

    Dodanie nowego xrodla bazy danych - widok

--- CarShowroomChain/FormCarSearch.cs
+++ CarShowroomChain/FormCarSearch.cs
@@ -25,17 +25,58 @@ namespace CarShowroomChain
             this.Close();
         }
 
-        private void buttonChoose_Click(object sender, EventArgs e) {
-            if (this.SelectCar == null) {
+        private void FormCarSearch_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'polsl_agatek_bdDataSet1.full_car' table. You can move, or remove it, as needed.
+            this.full_carTableAdapter.Fill(this.polsl_agatek_bdDataSet1.full_car);
+
+        }
+
+        private void dataGridViewCar_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            row = this.dataGridViewCar.Rows[e.RowIndex];
+        }
+
+        private void buttonChoose_Click(object sender, EventArgs e) 
+        {
+            if (this.SelectCar == null) //?
+            {
                 this.Close();
                 return;
             }
-            if (row != null) {
+            if (row != null) 
+            {
                 this.SelectCar(row);
                 this.Close();
                 return;
             } else
                 MessageBox.Show("Musisz najpierw wybrać samochód.");
         }
+
+        private void buttonFilter_Click(object sender, EventArgs e)
+        {
+            var model = this.comboBoxModel.Text;
+            /*
+            var name = this.textBoxName.Text;
+            var surname = this.textBoxSurname.Text;
+            var dbModel = new DatabaseModel();
+            string wherePart = " WHERE";
+            if (!String.IsNullOrWhiteSpace(name))
+                wherePart += " first_name like '" + name + "'";
+            if (!String.IsNullOrWhiteSpace(name) && !String.IsNullOrWhiteSpace(surname))
+                wherePart += " AND";
+            if (!String.IsNullOrWhiteSpace(surname))
+                wherePart += " last_name like '" + surname + "'";
+            if (wherePart.Equals(" WHERE"))
+                wherePart = "";
+            var db = dbModel.client.SqlQuery("SELECT * FROM client" + wherePart + ";").ToList();
+            var source = new BindingSource();
+            source.DataSource = db;
+            this.dataGridViewClients.DataSource = source;*/
+        }
+
+
+
+
     }
 }
